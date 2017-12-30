3e7dd61957643fac59185300f9d2d89679944b42
yalain03 <yalain9@gmail.com>
2017-12-30 12:10:03 -0600

    Angular app enabling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ValueComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
